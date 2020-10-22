464db058cf604910e99a654fb26eebb29d4763b5
Krunal Durne <krunaldurne91@gmail.com>
2020-10-22 14:30:21 +0530

    Updates

---
+++ precache-manifest.4bbb83a5c8aaa1096d5e83f9302584c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5045670c924f866a234df59f8b53da73",
+    "url": "/fb-clone/index.html"
+  },
+  {
+    "revision": "40e6dcae4f3fb720151b",
+    "url": "/fb-clone/static/css/main.f4a3f04c.chunk.css"
+  },
+  {
+    "revision": "63cd5c08d67217eefb3b",
+    "url": "/fb-clone/static/js/2.31915e15.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/fb-clone/static/js/2.31915e15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40e6dcae4f3fb720151b",
+    "url": "/fb-clone/static/js/main.7f07563c.chunk.js"
+  },
+  {
+    "revision": "70210a7fd678a62291cb",
+    "url": "/fb-clone/static/js/runtime-main.98fca3d7.js"
+  }
+]);
